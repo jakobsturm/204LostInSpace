6f436de253916e8a831fdb68d89346fd49fff9d1
Florian Bernhofer <florianbernhofer@gmx.de>
2019-03-01 09:40:22 +0100

    Added Entity

---
+++ LostInSpace/LostInSpaceLib/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostInSpaceLib
+{
+    abstract public class Entity
+    {
+        abstract public void Update();
+        abstract public void Draw();
+    }
+}
