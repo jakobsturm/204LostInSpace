a2edb2b5f980338caed7b3faed528466a6499518
MxHst <48118308+MxHst@users.noreply.github.com>
2019-03-01 11:16:17 +0100

    ItemManager added

---
+++ LostInSpace/LostInSpaceLib/Items/ItemManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace LostInSpaceLib.Items
+{
+    public class ItemManager
+    {
+
+
+
+        public int GeneratedRandom { get; set; }
+        Random rnd = new Random();
+
+
+        public ItemManager(GameTime gameTime, Rocket rocket)
+        {
+
+            ItemPicker(rocket.Position.Y);
+        }
+
+        public void ItemPicker(float RocketPosY)
+        {
+            GeneratedRandom = rnd.Next(1, 3);
+
+            switch (RocketPosY)
+            {
+                case 5000:
+
+                    break;
+
+                case 50000:
+
+                    break;
+
+                case 500000:
+
+                    break;
+
+                case 5000000:
+
+                    break;
+            }
+        }
+    }
+}
--- LostInSpace/LostInSpaceLib/Items/Money.cs
+++ LostInSpace/LostInSpaceLib/Items/Money.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace LostInSpaceLib
 {
     public class Money
     {
-        public Money()
+        
+
+        public Money(float posX, float posY)
         {
+            MoneyPosition = new Vector2(posX, posY);
+
 
         }
+
+        public Vector2 MoneyPosition { get; set; }
+
+
+
     }
 }
